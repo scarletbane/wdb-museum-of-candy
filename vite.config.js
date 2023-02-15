853e798ed58b684dffe0062a8aa80868aca805c3
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-02-14 20:04:23 -0700

    update starting template

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import htmlPurge from 'vite-plugin-purgecss';
 export default defineConfig({
     root: 'src',
     build: {
+        minify: 'esbuild',
         emptyOutDir: true,
         outDir: '../dist',
     },
     server: {
         hot: true,
+        open: true,
     },
     plugins: [htmlPurge()],
 });
