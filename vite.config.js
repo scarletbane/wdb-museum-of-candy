e9db78b93e4b2566495d4091dc74af091da3378f
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-02-14 18:11:31 -0700

    add purgecss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import htmlPurge from 'vite-plugin-purgecss';
 
 export default defineConfig({
     root: 'src',
+    build: {
+        emptyOutDir: true,
+        outDir: '../dist',
+    },
     server: {
         hot: true,
     },
+    plugins: [htmlPurge()],
 });
