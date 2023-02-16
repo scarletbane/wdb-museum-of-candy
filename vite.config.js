5d34f6497210fded67dcd4d065efea35f0b49227
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-02-15 21:07:36 -0700

    start project

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import htmlPurge from 'vite-plugin-purgecss';
+import stylelint from 'vite-plugin-stylelint';
+
+export default defineConfig({
+    root: 'src',
+    build: {
+        minify: 'esbuild',
+        emptyOutDir: true,
+        outDir: '../dist',
+    },
+    server: {
+        hot: true,
+        open: true,
+    },
+    plugins: [htmlPurge(), stylelint(), eslint()],
+});
