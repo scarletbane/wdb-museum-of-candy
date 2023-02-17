49796cc4670cfa6e6358c873c2f2b9d2ddebc481
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-02-16 18:48:28 -0700

    complete project

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+const mainNavbar = document.querySelector('#main-navbar');
+
+let windowScrolled = false;
+
+addEventListener('scroll', () => {
+    if (window.scrollY == 0) {
+        windowScrolled = false;
+    } else {
+        windowScrolled = true;
+    }
+
+    if (windowScrolled == true) {
+        mainNavbar.classList.add('scrolled');
+    } else {
+        mainNavbar.classList.remove('scrolled');
+    }
+});
+
+
